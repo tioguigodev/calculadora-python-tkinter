b5262196a0fa1079dc4d9f00792db13c9574f8b2
Guigo <rd.dossantos70@gmail.com>
2026-06-17 16:14:58 -0300

    Atualizar o calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,17 @@
 from tkinter import*
 
+# cores
+cor1 = '#3b3b3b'
+cor2 = '#FFAB40'
+cor3 = '#fffee3'
+cor4 = '#ab2500'
+cor5 = '#1e4226'
+cor6 = '#ebe9e4'
+cor7 = '#292929'
+
 janela = Tk()
 janela.title('Calculadora')
-
+janela.geometry('350x450')
+janela.resizable(False, False)
+janela.config(bg=cor1)
 janela.mainloop()
